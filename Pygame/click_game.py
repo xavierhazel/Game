4682b5ceb6d3862429584b53e69b77d4e1fff504
xavierhazel <113522425+xavierhazel@users.noreply.github.com>
2023-02-10 08:57:25 -0500

    Add files via upload

--- Pygame/click_game.py
+++ Pygame/click_game.py
@@ -1,26 +1,45 @@
-
-marsh=Actor('mallo')
-marsh.pos= 10,50
-
 WIDTH = 300
-HEIGHT= 300
+HIGHT = 300
+
 
 def draw():
-    screen.fill((128,0,0))
+    screen.fill((15, 56, 36))
+    screen.clear()
     marsh.draw()
 
 
+marsh = Actor("smore")
+marsh.pos = 100, 50
+marsh.topright = 0, 10
+WIDTH = 500
+HEIGHT = marsh.hight + 20
+
+
 def update():
-    marsh.left +=2
+    marsh.left += 2
     if marsh.left > WIDTH:
-       marsh.right=0
+        marsh.right = 0
 
-marsh.topright= 0,10
 
-    def on_mouse_down(pos):
-        if marsh.collidepoint(pos):
-        else:
-            pring("tu manque moi!")
-    def on_mouse_down():
-        print("tu clique")
+def on_mouse_down(pos):
+    print("tu clique")
+    if marsh.collidepoint(pos):
+        sounds.eep.play()
+        marsh.image = "mallo"
+        time.sleep(1)
+        marsh.image = "smore"
+        set_marsh_hurt
+    if button == mouse.LEFT and marsh.collidepoint(pos):
+        print("sacra bleu")
+    else:
+        print("tu manque moi!!")
 
+
+def set_marsh_hurt(pos):
+    marsh.image = "mallo"
+    sounds.eep.play()
+    clock.schedule_unique(set_marsh_normal, 1.0)
+
+
+def set_marsh_normal():
+    marsh.image = "smore"
